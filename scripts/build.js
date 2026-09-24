996cd6ed4ea9d66add74ab28e74c5ea53a5be14e
Timothy Bott <timothy.bott87@gmail.com>
2026-09-24 14:42:17 +0200

    d

--- scripts/build.js
+++ scripts/build.js
@@ -10,6 +10,8 @@ fs.mkdirSync(path.join(dist, 'styles'), { recursive: true });
 fs.mkdirSync(path.join(dist, 'scripts'), { recursive: true });
 fs.cpSync(path.join(root, 'assets'), path.join(dist, 'assets'), { recursive: true });
 fs.copyFileSync(path.join(root, 'index.html'), path.join(dist, 'index.html'));
+fs.copyFileSync(path.join(root, 'terms.html'), path.join(dist, 'terms.html'));
+fs.copyFileSync(path.join(root, 'privacy-policy.html'), path.join(dist, 'privacy-policy.html'));
 fs.copyFileSync(path.join(root, '404.html'), path.join(dist, '404.html'));
 fs.copyFileSync(path.join(root, 'robots.txt'), path.join(dist, 'robots.txt'));
 fs.copyFileSync(path.join(root, 'sitemap.xml'), path.join(dist, 'sitemap.xml'));
--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -10,6 +10,8 @@ const port = 4001;
 
 const watchedPaths = [
   'index.html',
+  'terms.html',
+  'privacy-policy.html',
   '404.html',
   'robots.txt',
   'sitemap.xml',
